148dbe705ab63e6df5836b885047ae8d556dcc6f
SDen4 <asw325@yandex.ru>
2022-05-29 17:40:28 +0500

    iteration with child component

--- src/app/test-card/test-card.component.ts
+++ src/app/test-card/test-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test-card',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestCardComponent implements OnInit {
   constructor() {}
+
+  @Input() userName: string = '';
+  @Input() userAge: number = 0;
 
   ngOnInit(): void {}
 }
--- src/app/test-component2/test-component2.component.ts
+++ src/app/test-component2/test-component2.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test-component2',
   templateUrl: './test-component2.component.html',
-  styleUrls: ['./test-component2.component.scss']
+  styleUrls: ['./test-component2.component.scss'],
 })
 export class TestComponent2Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  testArray: { name: string; age: number }[] = [
+    { name: 'Andrew', age: 43 },
+    { name: 'Mark', age: 49 },
+    { name: 'Lora', age: 61 },
+    { name: 'Migel', age: 58 },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
