4a4db1c86a77b24e2533d31b456d706af8030807
SDen4 <asw325@yandex.ru>
2022-05-29 17:21:41 +0500

    rest-card component draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { TestComponent2Component } from './test-component2/test-component2.component';
+import { TestCardComponent } from './test-card/test-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponentComponent,
-    TestComponent2Component
+    TestComponent2Component,
+    TestCardComponent
   ],
   imports: [
     BrowserModule,
