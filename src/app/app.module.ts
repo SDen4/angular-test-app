4818f1ee1993b2b6cae6d22fb262ea9f53e300d1
SDen4 <asw325@yandex.ru>
2022-06-02 11:47:09 +0500

    change structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
-import { TestComponent2Component } from './test-component2/test-component2.component';
-import { TestCardComponent } from './test-card/test-card.component';
-import { TestComponent3Component } from './test-component3/test-component3.component';
+import { TestComponentComponent } from './components/test-component/test-component.component';
+import { TestComponent2Component } from './components/test-component2/test-component2.component';
+import { TestCardComponent } from './components/test-card/test-card.component';
+import { TestComponent3Component } from './components/test-component3/test-component3.component';
 
 import { registerLocaleData } from '@angular/common';
 import LocaleRu from '@angular/common/locales/ru';
